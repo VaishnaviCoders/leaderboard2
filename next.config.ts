ef7653515ecbb81f317a954fcc28c100d6b565a8
VaishnaviCoders <vaishnaviraykar768@gmail.com>
2025-01-03 11:32:52 +0000

    config fix

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,7 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+
   headers: async () => {
     return [
       {
